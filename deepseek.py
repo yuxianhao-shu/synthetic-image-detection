0e1adc32a55ca2a4920efa1c995329f51bf2551a
learnerThomasyu <yuxianhao23@gmail.com>
2025-03-19 22:30:37 +0800

    Update deepseek.py

--- deepseek.py
+++ deepseek.py
@@ -4,12 +4,13 @@ from openai import OpenAI
 client = OpenAI(api_key="sk-9d7984a809594bf091dc6b27a28c69ab", base_url="https://api.deepseek.com")
 
 response = client.chat.completions.create(
-    model="deepseek-chat",
+    model="deepseek-reasoner",
     messages=[
-        {"role": "system", "content": "You are a dog"},
+        {"role": "system", "content": "You are a helpful assitant."},
         {"role": "user", "content": "Hello"},
     ],
     stream=False
 )
 
+print(response.choices[0].message.reasoning_content)
 print(response.choices[0].message.content)
